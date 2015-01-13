bd4b0189b436a89f844fde469ec84330d886439f
Ciro Santos <ciro.santos@synchro.com.br>
2015-01-13 09:33:58 -0200

    tste

--- src/test/java/br/com/synchro/jenkins/Imposto2Test.java
+++ src/test/java/br/com/synchro/jenkins/Imposto2Test.java
@@ -15,6 +15,7 @@ public class Imposto2Test {
 
     @Test
     public void deveCalcularDezPorcentoDeImpostoSobreQuinhentos() {
+	System.out.println("testeee.....");
 	final Orcamento orcamento = new Orcamento(500.0);
 	final Imposto imposto = new Imposto();
 	assertEquals(50.0, imposto.calcula(orcamento), Double.MIN_VALUE);
