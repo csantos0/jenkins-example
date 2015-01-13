a5a65599a22e22f7e551934fae3f6c4c413e8c15
Ciro Santos <ciro.santos@synchro.com.br>
2015-01-13 09:57:09 -0200

    tests
    Signed-off-by: Ciro Santos <ciro.santos@synchro.com.br>

--- src/test/java/br/com/synchro/jenkins/Imposto2Test.java
+++ src/test/java/br/com/synchro/jenkins/Imposto2Test.java
@@ -15,7 +15,7 @@ public class Imposto2Test {
 
     @Test
     public void deveCalcularDezPorcentoDeImpostoSobreQuinhentos() {
-	System.out.println("testeee.....");
+	System.out.println("testeee2.....");
 	final Orcamento orcamento = new Orcamento(500.0);
 	final Imposto imposto = new Imposto();
 	assertEquals(50.0, imposto.calcula(orcamento), Double.MIN_VALUE);
