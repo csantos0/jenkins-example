9e392a514b2d18553b0e42c769f85c5640cbcdff
Ciro Santos <ciro.santos@synchro.com.br>
2015-01-13 09:28:26 -0200

    testando o jenkins

--- src/test/java/br/com/synchro/jenkins/Imposto2Test.java
+++ src/test/java/br/com/synchro/jenkins/Imposto2Test.java
@@ -7,9 +7,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import br.com.synchro.jenkins.Imposto;
-import br.com.synchro.jenkins.Orcamento;
-
 /**
  * @author cvs
  * @create Jan 13, 2015
@@ -20,7 +17,7 @@ public class Imposto2Test {
     public void deveCalcularDezPorcentoDeImpostoSobreQuinhentos() {
 	final Orcamento orcamento = new Orcamento(500.0);
 	final Imposto imposto = new Imposto();
-	assertEquals(25.0, imposto.calcula(orcamento), Double.MIN_VALUE);
+	assertEquals(50.0, imposto.calcula(orcamento), Double.MIN_VALUE);
     }
 
 }
